17ea85c3eed55b48fdf27826b4f4249556faa560
Carlos Jose Toribio <cjtoribio@gmail.com>
2016-09-18 02:09:35 +0100

    - Fix sourceReferenceId it was removed from the select

--- endpoints/problems.js
+++ endpoints/problems.js
@@ -22,7 +22,7 @@ exports.up = function(ws, model){
 
         var query = model.Problem
         .find(ors.length ? {'$or': ors} : {})
-        .select('name url judge difficulty categories tags computedDifficulty')
+        .select('name url judge difficulty categories tags computedDifficulty sourceReferenceId')
         .sort({name: 1, _id: 1});
 
         if(req.query.limit)query.limit(req.query.limit);
