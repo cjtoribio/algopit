f4377c4ad83046c74772dec0054d4f7f50b5720e
Carlos Jose Toribio <cjtoribio@gmail.com>
2016-09-24 19:02:05 +0100

    Brush remove octal

--- utils/brush.js
+++ utils/brush.js
@@ -14,7 +14,7 @@ var colors = {
     yellow: [33, 39]
 };
 function colorize(txt, color){
-	return '\033[' + colors[color][0] + 'm' + txt + '\033[' + colors[color][1] + 'm';
+	return '\x1b[' + colors[color][0] + 'm' + txt + '\x1b[' + colors[color][1] + 'm';
 
 }
 
