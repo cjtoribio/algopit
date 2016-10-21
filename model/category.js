945dccecb2b7a529e5191bb8134a056de9f13cdc
Carlos Jose Toribio <cjtoribio@gmail.com>
2016-10-21 15:09:11 +0100

    Resolve issus #17 and #18

--- model/category.js
+++ model/category.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 var Category = new Schema({
-    name: String,
+    name: {type: String, default: ''},
     source: String,
     entryDate: { type: Date, default: Date.now }
 }, {
